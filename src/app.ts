1a31f5008415b15aa22cae0c07a72bfee4af3fd6
정훈박 <fireking5997@gmail.com>
2021-12-24 17:30:19 +0900

    Fix:드래그 구현중

--- src/app.ts
+++ src/app.ts
@@ -19,23 +19,62 @@ export interface IProjectList
 export interface IRenderItem
   extends BasicComponent<HTMLUListElement, HTMLLIElement> {
   renderList(): void;
+  setObserver: Ovserver;
+  addClass: (className: string) => void;
+  removeClass: (className: string) => void;
 }
+
+type State = "start" | "end" | "leave" | "enter";
+type Ovserver = (state: State, item: IRenderItem) => void;
 
 class RenderItem
   extends BasicComponent<HTMLUListElement, HTMLLIElement>
   implements IRenderItem
 {
+  private observer?: Ovserver;
   private project: Project;
   constructor(itemId: string, project: Project) {
     super("single-project", itemId, "afterbegin", project.id);
     this.project = project;
-
+    this.element.draggable = true;
     this.renderList();
+
+    this.element.addEventListener("dragstart", () => {
+      this.listener("start");
+    });
+
+    this.element.addEventListener("dragend", () => {
+      this.listener("end");
+    });
+
+    this.element.addEventListener("dragenter", () => {
+      this.listener("enter");
+    });
+
+    this.element.addEventListener("dragleave", () => {
+      this.listener("leave");
+    });
+  }
+
+  addClass(className: string) {
+    this.element.classList.add(className);
+  }
+
+  removeClass(className: string) {
+    this.element.classList.remove(className);
   }
 
   get getPeople(): string {
     const people = this.project.people;
     return people > 1 ? `${people}s` : people.toString();
+  }
+
+  set setObserver(func: Ovserver) {
+    this.observer = func;
+  }
+
+  private listener(state: State) {
+    this.observer && this.observer(state, this);
   }
 
   renderList(): void {
@@ -49,6 +88,10 @@ class ProjectList
   extends BasicComponent<HTMLDivElement, HTMLUListElement>
   implements IProjectList
 {
+  private lis = new Set<IRenderItem>();
+  private moving?: IRenderItem;
+  private target?: IRenderItem;
+
   private assignedState: Project[] = [];
   private ul: HTMLUListElement = this.element.querySelector("ul")!;
   constructor(
@@ -59,6 +102,15 @@ class ProjectList
     this.renderList();
 
     ManageState.getInstance().addListener(this.listener);
+
+    this.ul.addEventListener("drop", (event: DragEvent) => {
+      event.preventDefault();
+      console.log(this.moving, this.target);
+    });
+
+    this.ul.addEventListener("dragover", (event: DragEvent) => {
+      event.preventDefault();
+    });
   }
 
   @AutoBind
@@ -67,10 +119,42 @@ class ProjectList
     this.renderItem();
   }
 
+  private setMovingClass(className: string) {
+    this.lis.forEach((item) => item.addClass(className));
+  }
+
+  private removeMovingClass(className: string) {
+    this.lis.forEach((item) => item.removeClass(className));
+  }
+
+  @AutoBind
+  private observer(state: State, item: IRenderItem) {
+    switch (state) {
+      case "end":
+        this.removeMovingClass("moving");
+        this.moving = undefined;
+        break;
+      case "start":
+        this.setMovingClass("moving");
+        this.moving = item;
+        break;
+      case "leave":
+        this.target = undefined;
+        break;
+      case "enter":
+        this.target = item;
+        break;
+      default:
+        throw new Error("잘못된 상태 타입 입니다.");
+    }
+  }
+
   private renderItem() {
     this.ul.innerHTML = "";
     for (const item of this.assignedState) {
-      new this.constractor(this.ul.id, item);
+      const jtem = new this.constractor(this.ul.id, item);
+      this.lis.add(jtem);
+      jtem.setObserver = this.observer;
     }
   }
 
