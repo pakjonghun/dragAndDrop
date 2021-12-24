7ae8d6f19df85b445375e16009f9441fcc1204c7
정훈박 <fireking5997@gmail.com>
2021-12-24 22:51:14 +0900

    Fix:웹팩 서버 돌리려면 서버 옵션 설정해줘야 한다. html 파일이 있는 폴더로!

--- webpack.config.js
+++ webpack.config.js
@@ -13,4 +13,11 @@ module.exports = {
   resolve: {
     extensions: [".ts", ".js"],
   },
+  devServer: {
+    static: {
+      directory: path.join(__dirname),
+    },
+    compress: true,
+    port: 9000,
+  },
 };
